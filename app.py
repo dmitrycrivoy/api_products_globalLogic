cae913cfbc90fe9f728d3c007485cfae219db096
Dmitry Crivoy <dmitrycrivoy@gmail.com>
2022-05-12 12:38:22 +0300

    changes 12.05.2022 12:38

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 from flask import Flask, send_file, request, json
 
-app = Flask(__name__)
+JSON_PATH = "data.json"
 
+app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/', methods = ['GET', 'POST'])
 def products():
     if request.method == 'GET':
-        return send_file('data.json')
+        return send_file(JSON_PATH)
     if request.method == 'POST':
         return "POST RESPONSE"
 
-@app.route('/<product_id>', methods = ['GET', 'POST'])
+@app.route('/buy/<product_id>', methods = ['GET', 'POST'])
 def product_by_id(product_id):
-    if request.method == 'GET':
-        with open("data.json") as json_file:
-            data = json.load(json_file)
-            for product in data:
-                if product["id"] == product_id:
-                    return product
+    # if request.method == 'GET':
+        # with open(JSON_PATH) as json_file:
+        #     data = json.load(json_file)
+        #     for product in data:
+        #         if product["id"] == product_id:
+        #             return product
     if request.method == 'POST':
-        return "POST RESPONSE"
+        with open(JSON_PATH) as json_file:
+            data = json.load(json_file)
+            product_by_id = ''
+            for product in data:
+                if product["id"] == int(product_id):
+                    product_by_id = product
+            if not product_by_id:
+                return {"error": "No product by this id"}
+            else:
+                return product_by_id
 
 if __name__ == '__main__':
     app.run(debug=True)
--- request.py
+++ request.py
@@ -1,32 +1,71 @@
-import requests
+import requests, sys
 
-def check_user_input(input):
+BASE_URL = "http://127.0.0.1:5000/"
+BUY_PATH = "buy/"
+
+def check_is_number(input):
     try:
         value = int(input)
         return value
     except ValueError:
-        print("Please enter correct number")
+        print("Please, enter correct number")
+        sys.exit()
+
+def print_menu():
+    print("Please, choose one of the options:")
+    menu = [
+        "1. Checking the status in the shop",
+        "2. Buying products"
+    ]
+    for menu_item in menu:
+        print(menu_item)
+    item_of_menu_input = input("Your selection: ")
+    print(item_of_menu_input)
+    item_of_menu = check_is_number(item_of_menu_input)
+    if item_of_menu > len(menu):
+        print(f"\nPlease enter number from 1 to {len(menu)}")
+    return item_of_menu
+
+def make_table(data):
+    if "error" in data:
+        return print(data)
+    else:
+        print("{:<4} {:<12} {:<5}".format("ID", "NAME", "QUANTITY"))
+        table = ''
+        if type(data) == dict:
+            table += "{:<4} {:<12} {:<5}".format(data["id"], data["product"], 
+                                            data["remaining_quantity"]) + "\n"
+        elif type(data) == list:
+            for i in data:
+                table += "{:<4} {:<12} {:<5}".format(i["id"], i["product"], 
+                                            i["remaining_quantity"]) + "\n"
+        return print(table)
+
+def request(selection):
+    if selection == 1:
+        response = requests.get(BASE_URL)
+        data = response.json()
+        print()
+        make_table(data)
+        print()
+    elif selection == 2:
+        product_id = input("\nEnter id of product: ")
+        check_is_number(product_id)
+        full_url = BASE_URL + BUY_PATH + product_id
+        response = requests.post(full_url)
+        data = response.json()
+        make_table(data)
 
 
-BASE_URL = "http://127.0.0.1:5000/"
+def main():
+    selection = print_menu()
+    while True:
+        request(selection)
+        selection = print_menu()
 
-print("\nPlease, choose one of the options:")
-print("1. Checking the status in the shop")
-print("2. Buying products")
-item_of_menu_input = input()
-item_of_menu = check_user_input(item_of_menu_input)
-
-
-if item_of_menu == 1:
-    response = requests.get(BASE_URL)
-    data = response.json()
-    print()
-    print("{:<4} {:<12} {:<5}".format("ID", "NAME", "QUANTITY"))
-    for i in data:
-        print("{:<4} {:<12} {:<5}".format(i["id"], i["product"], i["remaining_quantity"]))
-    print()
-
-elif item_of_menu == 2:
-    response = requests.post(BASE_URL)
-    data = response.text
-    print(data)
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nInterrupted by user')
+        sys.exit()
