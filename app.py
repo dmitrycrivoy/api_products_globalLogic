6b2e2131ce1478c5992d13bc956c8c14cffc3f24
Dmitry Crivoy <dmitrycrivoy@gmail.com>
2022-05-12 20:23:06 +0300

    changes 12.05.2022 20:22

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, send_file, request, json
+from flask import Flask, send_file, request, json, jsonify
 
 JSON_PATH = "data.json"
+
+def check_positive_number(input):
+    try:
+        value = int(input)
+        if value <= 0:
+            return { "error": "Please, enter correct number"}
+        else:
+            return value
+    except ValueError:
+        return { "error": "Please, enter correct number"}
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,17 +20,15 @@ def products():
     if request.method == 'GET':
         return send_file(JSON_PATH)
     if request.method == 'POST':
-        return "POST RESPONSE"
+        return {"error": "Use GET request instead"}
 
-@app.route('/buy/<product_id>', methods = ['GET', 'POST'])
-def product_by_id(product_id):
-    # if request.method == 'GET':
-        # with open(JSON_PATH) as json_file:
-        #     data = json.load(json_file)
-        #     for product in data:
-        #         if product["id"] == product_id:
-        #             return product
+@app.route('/buy', methods = ['GET', 'POST'])
+def product_by_id():
+    if request.method == 'GET':
+        return {"error": "Use POST request instead"}
     if request.method == 'POST':
+        product_id = check_positive_number(request.json["product_id"])
+        buy_amount = check_positive_number(request.json["buy_amount"])
         with open(JSON_PATH) as json_file:
             data = json.load(json_file)
             product_by_id = ''
@@ -28,9 +36,21 @@ def product_by_id(product_id):
                 if product["id"] == int(product_id):
                     product_by_id = product
             if not product_by_id:
-                return {"error": "No product by this id"}
+                return {"error": "No product by id"}
             else:
-                return product_by_id
+                if product_by_id["quantity"] >= int(buy_amount):
+                    product_by_id["quantity"] -= int(buy_amount)
+                    with open(JSON_PATH, 'w') as json_file:
+                        json.dump(data, json_file, indent=4)
+                    return {
+                        "success": 
+                        f'you bought {buy_amount} of "{product_by_id["product"]}"'
+                    }
+                else:
+                    return {
+                        "error": 
+                        f'Not enough product \""{product_by_id["product"]}"\" in the warehouse'
+                    }
 
 if __name__ == '__main__':
     app.run(debug=True)
--- request.py
+++ request.py
@@ -1,46 +1,58 @@
 import requests, sys
 
-BASE_URL = "http://127.0.0.1:5000/"
-BUY_PATH = "buy/"
+CRED = '\033[91m'
+CEND = '\033[0m'
 
-def check_is_number(input):
+BASE_URL = "http://127.0.0.1:5000/"
+BUY_PATH = "buy"
+
+menu = [
+    "1. Checking the status in the shop",
+    "2. Buying products",
+    "3. Exit"
+]
+
+def check_positive_number(input):
     try:
         value = int(input)
-        return value
+        if value <= 0:
+            print("\n" + CRED + "Please, enter correct number" + CEND)
+            return
+        else:
+            return value
     except ValueError:
-        print("Please, enter correct number")
-        sys.exit()
+        print("\n" + CRED + "Please, enter correct number" + CEND)
+        return
 
 def print_menu():
-    print("Please, choose one of the options:")
-    menu = [
-        "1. Checking the status in the shop",
-        "2. Buying products"
-    ]
+    print("\nPlease, choose one of the options:")
     for menu_item in menu:
         print(menu_item)
+
+def select_menu_item():
     item_of_menu_input = input("Your selection: ")
-    print(item_of_menu_input)
-    item_of_menu = check_is_number(item_of_menu_input)
-    if item_of_menu > len(menu):
-        print(f"\nPlease enter number from 1 to {len(menu)}")
+    item_of_menu = check_positive_number(item_of_menu_input)
+    if item_of_menu == None:
+        return
+    elif item_of_menu > len(menu):
+        print(f"\n{CRED}Please enter number from 1 to {len(menu)}{CEND}")
     return item_of_menu
 
 def print_data(data):
-    if "error" in data:
-        return print(data)
-    else:
-        table_format = "{:<4} {:<12} {:<5}"
-        print(table_format.format("ID", "NAME", "QUANTITY"))
-        table = ''
-        if type(data) == dict:
-            table += table_format.format(data["id"], data["product"], 
-                                            data["remaining_quantity"]) + "\n"
-        elif type(data) == list:
-            for i in data:
-                table += table_format.format(i["id"], i["product"], 
-                                            i["remaining_quantity"]) + "\n"
-        return print(table)
+    for status in ["success", "error"]:
+        if status in data:
+            return print(data)
+    table_format = "{:<4} {:<12} {:<5}"
+    print(table_format.format("ID", "NAME", "QUANTITY"))
+    table = ''
+    if type(data) == dict:
+        table += table_format.format(data["id"], data["product"], 
+                                     data["quantity"]) + "\n"
+    elif type(data) == list:
+        for i in data:
+            table += table_format.format(i["id"], i["product"], 
+                                         i["quantity"]) + "\n"
+    return print(table)
 
 def request(selection):
     if selection == 1:
@@ -48,21 +60,32 @@ def request(selection):
         data = response.json()
         print()
         print_data(data)
-        print()
     elif selection == 2:
         product_id = input("\nEnter id of product: ")
-        check_is_number(product_id)
-        full_url = BASE_URL + BUY_PATH + product_id
-        response = requests.post(full_url)
+        check_positive_number(product_id)
+        buy_product_amount = input("\nEnter an amount of the " 
+                                   + "product you want to buy: ")
+        check_positive_number(buy_product_amount)
+
+        buy_url = BASE_URL + BUY_PATH
+        request_data = {
+            "product_id": product_id,
+            "buy_amount": f"{buy_product_amount}"
+        }
+        # headers = {'Content-Type': 'application/json'}
+        # response = requests.post(buy_url, headers=headers, json=request_data)
+        response = requests.post(buy_url, json=request_data)
         data = response.json()
         print_data(data)
 
 
 def main():
-    selection = print_menu()
-    while True:
+    print_menu()
+    selection = select_menu_item()
+    while selection != 3:
         request(selection)
-        selection = print_menu()
+        print_menu()
+        selection = select_menu_item()
 
 if __name__ == '__main__':
     try:
